b5e212c7b5682faf3109ee98fcacb5644fa7ac02
seominseok0429 <king_v_@naver.com>
2020-11-29 11:23:07 +0900

    queryset

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
+def post_list(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(content__icontains=q)
+    return render(request, 'blog/post_list.html',{
+        'post_list':qs,
+        'q':q         
+    })
 # Create your views here.
